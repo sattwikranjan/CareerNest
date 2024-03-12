2734a53054399a191aea4e49429709f63c26e290
Sattwik Ranjan <sattwikranjan@gmail.com>
2024-03-12 17:40:44 +0530

    error handler and not found middleware

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
 
+//middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+notFoundMiddleware;
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
