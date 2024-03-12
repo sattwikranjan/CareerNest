4d5d42fbce9cb90401927aa17ee4455e76984cd0
Sattwik Ranjan <sattwikranjan@gmail.com>
2024-03-12 18:01:38 +0530

    database connecteed and environment

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 const app = express();
 
+import dotenv from "dotenv";
+dotenv.config();
+
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
 notFoundMiddleware;
 
 app.get("/", (req, res) => {
@@ -15,6 +19,19 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}....`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}....`);
+// });
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
