7438500d2dd01de95fcf57f9b968ff98cddee4c4
Ranoo <kumari.ranu525@gmail.com>
2024-03-14 11:54:48 +0800

    Connect frontend and backend

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,24 @@
+import React,{ useEffect } from 'react';
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const fetchData=async()=>{
+    try{
+    const response=await fetch('/api/v1')
+    const data=await response.json()
+    console.log(data);
+    }
+    catch (error){
+      console.log(error);
+    }
+  
+  }
+  useEffect(()=>{
+    fetchData()
+      
+    }
+  ,[])
+  return <h1>Dashboard
+  </h1>;
 };
 
 export default Dashboard;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
@@ -15,11 +16,18 @@ import jobsRouter from "./routes/jobsRouter.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+app.get("/api/v1", (req, res) => {
+  req.json({msg: 'Welcome!'});
+});
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
