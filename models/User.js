97491a1671cfd6eae9f82c778c140964f593c027
Sattwik Ranjan <sattwikranjan@gmail.com>
2024-03-16 00:41:07 +0530

    Bug fix and update user controller

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -23,8 +24,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
-    select:false,
-    
+    select: false,
   },
   lastName: {
     type: String,
@@ -40,22 +40,23 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre('save',async function(){
-  const salt=await bcrypt.genSalt(10);
-  this.password=await bcrypt.hash(this.password,salt)
-})
+UserSchema.pre("save", async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified('name'));
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-UserSchema.methods.createJWT=function(){
-  return jwt.sign({ userId:this._id }, process.env.JWT_SECRET, {
-    expiresIn:process.env.JWT_LIFETIME,
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
   });
-}
+};
 
-UserSchema.methods.comparePassword = async function( candidatePassword ){
-  const isMatch = await bcrypt.compare( candidatePassword, this.password);
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
   return isMatch;
-}
-
-
+};
 
 export default mongoose.model("User", UserSchema);
